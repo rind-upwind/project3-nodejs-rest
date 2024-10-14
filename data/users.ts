38f0b05b4e0c5b1cd83a1d6b47c6f07b9fe340af
rind-upwind <rind-kisser-upwind@duck.com>
2024-10-14 15:06:10 +0200

    Create users.ts

---
+++ data/users.ts
@@ -0,0 +1,27 @@
+export default [
+	{
+		ID: 1,
+		firstName: "John",
+		lastName: "Doe",
+	},
+	{
+		ID: 2,
+		firstName: "Jane",
+		lastName: "Smith",
+	},
+	{
+		ID: 3,
+		firstName: "Michael",
+		lastName: "Johnson",
+	},
+	{
+		ID: 4,
+		firstName: "Emily",
+		lastName: "Davis",
+	},
+	{
+		ID: 5,
+		firstName: "Chris",
+		lastName: "Brown",
+	},
+];
